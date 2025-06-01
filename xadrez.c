1433561b35409ecac7a9a91e9ab5492d26fcbdf4
Enivaldo Oliveira <enidogithub@gmail.com>
2025-06-01 18:38:09 -0300

    Criei o Xadrez.c
    Nível básico

---
+++ xadrez.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+//Sistema de movimentação Xadrez Enivaldo Oliveira
+int main() {
+    //Movimentação da Torre com For
+    printf("Movimento da Torre:\n");
+    for(int casas_torre = 0; casas_torre < 5; casas_torre++){
+        printf("Direita\n");
+    }
+    printf("\n");
+    
+    //Movimentação do Bisco com While
+    printf("Movimento do Bispo:\n");
+    int casas_bispo = 0;
+    while(casas_bispo < 5){
+        printf("Cima, direita\n");
+        casas_bispo++;
+    }
+    printf("\n");
+    
+    //Movimentação da Rainha com do-While
+    printf("Movimento da Rainha:\n"); 
+    int casas_rainha = 0;
+    do{
+        printf("Esquerda\n");
+        casas_rainha++;
+    }while (casas_rainha <8);
+    
+    return 0;
+}
